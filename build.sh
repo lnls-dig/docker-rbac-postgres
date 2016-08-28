ed2a66c17c976a699ae54b149259988ce23ae2b6
Lucas Russo <lerwys@gmail.com>
2016-08-28 13:17:49 -0300

    build.sh: add bash error/uninitialized/export

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -a
+set -u
+set -e
+
 # Change this if your Host have a "sane" DNS like 168.192.1.1
 OPTNAMESERVER="echo nameserver 10.0.0.71 > /etc/resolv.conf \&\& \\\\"
 #OPTNAMESERVER="\\\\"
