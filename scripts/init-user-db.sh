ac5140d2ec2d07c27e3c9175974c1ddfe12df6b6
Lucas Russo <lerwys@gmail.com>
2016-08-30 23:15:10 -0300

    scripts/init-user-db.sh: ignore duplicate insertion errors

--- scripts/init-user-db.sh
+++ scripts/init-user-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER rbac;
     CREATE DATABASE rbac;
     GRANT ALL PRIVILEGES ON DATABASE rbac TO rbac;
