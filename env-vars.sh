d887f2b6e7687b8b4ebb9c1cfb62d8ad85864642
Lucas Russo <lerwys@gmail.com>
2016-08-30 22:45:59 -0300

    env-vars.sh: differentiate between auth-services and management-studio

--- env-vars.sh
+++ env-vars.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
 
-NET_NAME="postgres-rbac"
+SERVICE="$1"
+
+# Default variables
+NET_NAME="postgres-rbac-auth-services"
 DNS_IP="10.0.0.72"
 POSTGRES_PORT=5432
+LOCAL_POSTGRES_PORT=5432
 POSTGRES_USER="rbac"
 POSTGRES_PASSWORD="rbac"
+RBAC_SERVICE_NAME=auth-services-postgres
+RBAC_DOCKER_IMAGE_NAME=rbac-auth-services-postgres
+RBAC_DOCKER_VOLUME=rbac-auth-services-postgres-volume
+
+if [ "${SERVICE}" == "rbac" ]; then
+    NET_NAME="postgres-rbac-auth-services"
+    RBAC_SERVICE_NAME=auth-services-postgres
+    RBAC_DOCKER_IMAGE_NAME=rbac-auth-services-postgres
+    RBAC_DOCKER_VOLUME=rbac-auth-services-postgres-volume
+    LOCAL_POSTGRES_PORT=5432
+elif [ "${SERVICE}" == "mgmt" ]; then
+    NET_NAME="postgres-rbac-management-studio"
+    RBAC_SERVICE_NAME=management-studio-postgres
+    RBAC_DOCKER_IMAGE_NAME=rbac-management-studio-postgres
+    RBAC_DOCKER_VOLUME=rbac-management-studio-postgres-volume
+    LOCAL_POSTGRES_PORT=5433
+else
+    echo "Unsupported service"
+    exit 1
+fi
