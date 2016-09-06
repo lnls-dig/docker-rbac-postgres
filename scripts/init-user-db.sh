2f8b374505d3be00672df5874441fc9345911896
Lucas Russo <lerwys@gmail.com>
2016-09-06 16:07:36 -0300

    scripts/init-user-db.sh: avoid recreating databases
    The default is to create a database with
    the same name as "POSTGRES_USER"

--- scripts/init-user-db.sh
+++ scripts/init-user-db.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e
 
-psql -v --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER rbac;
-    CREATE DATABASE rbac;
-    GRANT ALL PRIVILEGES ON DATABASE rbac TO rbac;
-EOSQL
+# Nothing to do
