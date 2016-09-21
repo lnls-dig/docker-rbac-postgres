1324ef8b72ad2e4bc95804a30c51468100d13fad
Lucas Russo <lerwys@gmail.com>
2016-09-21 11:05:09 -0300

    run-postgres.sh: fix run docker name

--- run-postgres.sh
+++ run-postgres.sh
@@ -9,7 +9,7 @@ SERVICE="$1"
 . ./env-vars.sh ${SERVICE}
 
 # Run postgres_user
-docker run  --name ${RBAC_DOCKER_IMAGE_NAME} --net ${NET_NAME} --dns ${DNS_IP} \
+docker run  --name ${RBAC_DOCKER_RUN_NAME} --net ${NET_NAME} --dns ${DNS_IP} \
     -p ${LOCAL_POSTGRES_PORT}:${POSTGRES_PORT} -e POSTGRES_USER=${POSTGRES_USER} \
     -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
     -d --volumes-from ${RBAC_DOCKER_VOLUME} ${RBAC_DOCKER_ORG_NAME}/${RBAC_DOCKER_IMAGE_NAME}
