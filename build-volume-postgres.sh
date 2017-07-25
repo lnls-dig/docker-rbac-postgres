a0cb592255f5a003a80cabdc33532ee55b0ac531
Lucas Russo <lerwys@gmail.com>
2017-07-25 16:35:23 -0300

    build-volume-postgres.sh: create data-only volume before container volume

--- build-volume-postgres.sh
+++ build-volume-postgres.sh
@@ -9,5 +9,6 @@ SERVICE="$1"
 . ./env-vars.sh ${SERVICE}
 
 # Create volume container
-docker create -v /var/lib/postgresql/data --name ${RBAC_DOCKER_VOLUME} \
-    --net ${NET_NAME} --dns ${DNS_IP} postgres 2>/dev/null || true
+docker volume create ${RBAC_DOCKER_VOLUME}
+docker create -v ${RBAC_DOCKER_VOLUME}:/var/lib/postgresql/data --name ${RBAC_DOCKER_VOLUME} \
+    --net ${NET_NAME} --dns ${DNS_IP} postgres:9.5.4 2>/dev/null || true
